9e81bee8bfb6265a9940edb56fc3dd0205211b50
Stephanie B <stephanie.s.bello@gmail.com>
2018-12-29 09:37:12 -0600

    Modified App js file and renamed img to images

--- js/app.js
+++ js/app.js
@@ -1,46 +1,124 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var character = ['boy', 'cat-girl', 'horn-girl', 'pink-girl', 'princess-girl'];
+//var charPick = Math.floor(Math.random() * 5);
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
+    // The following variables are used to determine the x and y axis and speed of the enemy
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image of the enemy of cockroach that is added to the playing field
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+Enemy.prototype.update = function (dt) {
+    // Multiplies the speed by the dt parameter on the x axis
+    this.x += this.speed * dt;
+
+    // Once enemies are off the canvas, they reappear randomly with different speeds
+    if (this.x > 505) {
+        this.x = -10;
+        this.speed = 75 + Math.floor(Math.random() * 500);
+    };
+
+    // Checks for collisions between the player and the enemies
+    if ((player.x < this.x + 70) && (player.x + 70 > this.x) && (player.y < this.y + 50) && (50 + player.y > this.y)) {
+        player.x = 202;
+        player.y = 405;
+    };
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+// Renders the enemy into the game
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class focusing on x and y axis
+var Player = function (x, y) {
+    // Variables for the player to move along x and y axis
+    this.x = x;
+    this.y = y;
+
+    //The image of the player is added to the playing field
+    var charPick = Math.floor(Math.random() * 5);
+    this.player = 'images/char-'.concat(character[charPick], '.png');
+};
+
+Player.prototype.update = function (dt) {
+
+};
+
+// Renders the image of the user into the game
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
+
+// Allows the user to use the arrow keys to jump from tile to tile
+Player.prototype.handleInput = function (keyPress) {
+
+    // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
+    if (keyPress == 'left' && player.x > 2) {
+        player.x -= 100;
+    };
+
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
+    if (keyPress == 'right' && player.x < 400) {
+        player.x += 100;
+    };
+
+    // Enables user on up arrow key to move upwards on the y axis by 83
+    if (keyPress == 'up' && player.y > -10) {
+        player.y -= 84;
+    };
+
+    // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
+    if (keyPress == 'down' && player.y < 400) {
+        player.y += 84;
+
+    };
+
+    // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position
+    if (this.y < 0) {
+        setTimeout(() => {
+            this.x = 200;
+            this.y = 400;
+        }, 400);
+    };
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// All enemies are placed in an array
+var allEnemies = [];
+
+// Location of the 3 enemies on the y axis located on the stone road
+var enemyLocation = [63, 147, 230];
 
 
+// For each enemy located on the y axis from 0 on the x axis move at a speed of 200
+// Until randomly regenerated in the enemy update function above
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+
+// The starting location of the player is located at x=200, y=405
+var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// Player.handleInput() method.
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
